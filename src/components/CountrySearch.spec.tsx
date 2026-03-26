664ee5326b1d3ec6b1acd11cb98c5f9c2ef10a83
Luca Lischetti <luca@sirlisko.com>
2026-03-26 11:45:53 +0000

    chore(CI): add basic GHA

--- src/components/CountrySearch.spec.tsx
+++ src/components/CountrySearch.spec.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, expect, it, vi } from "vitest";
-import CountrySearch from "./CountrySearch";
 import type { CountryCode } from "../types";
+import CountrySearch from "./CountrySearch";
 
 const defaultProps = {
 	value: "",
